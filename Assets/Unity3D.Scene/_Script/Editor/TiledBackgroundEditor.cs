d00df44d4e164db531749e9f7d7d02a53360a172
0x600d1dea <xiaobin.huang@gmail.com>
2017-06-24 20:18:59 +0800

    added tiled background for big tiles that not in one atlas

---
+++ Assets/Unity3D.Scene/_Script/Editor/TiledBackgroundEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+namespace scene
+{
+	[CustomEditor(typeof(scene.TiledBackground))]
+	public class TiledBackgroundEditor : Editor
+	{
+		void OnSceneGUI()
+		{
+			var tiled = target as TiledBackground;
+
+		}
+
+	}
+}
---
+++ Assets/Unity3D.Scene/_Script/TiledBackground.cs
@@ -0,0 +1,310 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace scene
+{
+	public class TiledBackground : MonoBehaviour
+	{
+		public Material material;
+
+		Camera cam;
+		int ID_MainTex;
+
+		public interface TileProvider
+		{
+			bool showAllTiles { get; }
+			Texture GetTile(int tileX, int tileY);
+			Texture[] GetAllTiles(out int tileX0, out int tileY0, out int tileX1, out int tileY1);
+		}
+
+		public void SetCamera(Camera camera)
+		{
+			cam = camera;
+		}
+
+		public TileProvider tileProvider;
+
+
+		Mesh CreateMesh(int xcount, int ycount, string name)
+		{
+			var mesh = new Mesh();
+			mesh.name = name;
+			mesh.hideFlags = HideFlags.DontSave;
+
+			var vertices = new List<Vector3>();
+			var uvs = new List<Vector2>();
+			var triangles = new List<int[]>();
+			int baseIndex = 0;
+			for (int j = 0; j < ycount; ++j)
+			{
+				for (int i = 0; i < xcount; ++i)
+				{
+					// quad
+					float x0 = i, x1 = i + 1;
+					float z0 = j, z1 = j + 1;
+
+					vertices.Add(new Vector3(x0, 0, z0));
+					vertices.Add(new Vector3(x1, 0, z0));
+					vertices.Add(new Vector3(x1, 0, z1));
+					vertices.Add(new Vector3(x0, 0, z1));
+
+					uvs.Add(new Vector2(0, 0));
+					uvs.Add(new Vector2(1, 0));
+					uvs.Add(new Vector2(1, 1));
+					uvs.Add(new Vector2(0, 1));
+
+					triangles.Add(new int[] {
+						baseIndex + 0,
+						baseIndex + 2,
+						baseIndex + 1,
+						baseIndex + 0,
+						baseIndex + 3,
+						baseIndex + 2});
+
+					baseIndex += 4;
+				}
+			}
+
+			mesh.SetVertices(vertices);
+			mesh.SetUVs(0, uvs);
+			mesh.subMeshCount = triangles.Count;
+			for (int i = 0; i < triangles.Count; ++i)
+			{
+				mesh.SetTriangles(triangles[i], i);
+			}
+			mesh.RecalculateBounds();
+			mesh.RecalculateNormals();
+
+			return mesh;
+		}
+
+		GameObject allTilesMeshObj;
+		void CreateAllTiles()
+		{
+			if (tileProvider != null)
+			{
+				if (allTilesMeshObj != null)
+				{
+					Destroy(allTilesMeshObj);
+				}
+
+				int tileX0, tileY0, tileX1, tileY1;
+				var textures = tileProvider.GetAllTiles(out tileX0, out tileY0, out tileX1, out tileY1);
+
+				allTilesMeshObj = new GameObject("All Tiles Mesh Object");
+				allTilesMeshObj.hideFlags = HideFlags.DontSave;
+				allTilesMeshObj.transform.SetParent(transform, false);
+
+				var xcount = tileX1 - tileX0;
+				var ycount = tileY1 - tileY0;
+				var mesh = CreateMesh(xcount, ycount, "All Tiles Mesh");
+				var materials = new Material[xcount * ycount];
+				for (int i = 0; i < materials.Length; ++i)
+				{
+					materials[i] = new Material(material);
+					materials[i].name = materials[i].name + "(Clone)";
+					materials[i].hideFlags = HideFlags.DontSave;
+					materials[i].SetTexture(ID_MainTex, textures[i]);
+				}
+
+				var meshRenderer = allTilesMeshObj.AddComponent<MeshRenderer>();
+				meshRenderer.hideFlags = HideFlags.DontSave;
+				meshRenderer.sharedMaterials = materials;
+
+				var meshFilter = allTilesMeshObj.AddComponent<MeshFilter>();
+				meshFilter.hideFlags = HideFlags.DontSave;
+				meshFilter.sharedMesh = mesh;
+			}
+		}
+
+
+		Material[] materials;
+		GameObject meshObj;
+		int meshTileXCount;
+		int meshTileYCount;
+		void CreateMeshAndMaterial(int visibleTileXCount, int visibleTileYCount)
+		{
+			Debug.Assert(visibleTileXCount > 0 && visibleTileXCount > 0);
+			Debug.Assert(material != null);
+			meshTileXCount = visibleTileXCount + 1;
+			meshTileYCount = visibleTileYCount + 1;
+
+			if (meshObj != null)
+			{
+				Destroy(meshObj);
+				materials = null;
+				meshObj = null;
+			}
+
+			meshObj = new GameObject("Mesh Object");
+			meshObj.hideFlags = HideFlags.DontSave;
+			meshObj.transform.SetParent(transform, false);
+
+			var mesh = CreateMesh(meshTileXCount, meshTileYCount, "Tiles Mesh");
+
+			materials = new Material[meshTileXCount * meshTileYCount];
+			for (int i = 0; i < materials.Length; ++i)
+			{
+				materials[i] = new Material(material);
+				materials[i].name = materials[i].name + "(Clone)";
+				materials[i].hideFlags = HideFlags.DontSave;
+			}
+
+			var meshRenderer = meshObj.AddComponent<MeshRenderer>();
+			meshRenderer.hideFlags = HideFlags.DontSave;
+			meshRenderer.sharedMaterials = materials;
+
+			var meshFilter = meshObj.AddComponent<MeshFilter>();
+			meshFilter.hideFlags = HideFlags.DontSave;
+			meshFilter.sharedMesh = mesh;
+
+		}
+		void Awake()
+		{
+			ID_MainTex = Shader.PropertyToID("_MainTex");
+		}
+
+		void Start()
+		{
+			int tileX0, tileY0, tileX1, tileY1;
+			GetTileParams(out tileX0, out tileY0, out tileX1, out tileY1);
+			CreateMeshAndMaterial(tileX1 - tileX0, tileY1 - tileY0);
+		}
+
+
+		void GetProjParams(Camera c, out Vector3 wCamMinOnPlane, out Vector3 wCamMaxOnPlane)
+		{
+			if (c.orthographic)
+			{
+				var camPos = c.transform.position;
+				var size = (c.transform.up + c.transform.right * c.aspect) * c.orthographicSize;
+
+				var camMin = camPos - size;
+				var camMax = camPos + size;
+				camMin.x = Mathf.Min(camMin.x, camMax.x);
+				camMin.y = Mathf.Min(camMin.y, camMax.y);
+				camMin.z = Mathf.Min(camMin.z, camMax.z);
+
+				camMax.x = Mathf.Max(camMin.x, camMax.x);
+				camMax.y = Mathf.Max(camMin.y, camMax.y);
+				camMax.z = Mathf.Max(camMin.z, camMax.z);
+
+				var plane = new Plane(transform.up, transform.position);
+				float enter;
+				var r = new Ray(camMin, c.transform.forward);
+				plane.Raycast(r, out enter);
+				wCamMinOnPlane = r.GetPoint(enter);
+				r = new Ray(camMax, c.transform.forward);
+				plane.Raycast(r, out enter);
+				wCamMaxOnPlane = r.GetPoint(enter);
+
+			}
+			else
+			{
+				wCamMinOnPlane = Vector3.zero;
+				wCamMaxOnPlane = Vector3.zero;
+			}
+		}
+
+		void GetTileParams(out int tileX0, out int tileY0, out int tileX1, out int tileY1)
+		{
+			var c = cam;
+			if (c == null)
+			{
+				c = Camera.main;
+			}
+			Vector3 camMinOnPlane, camMaxOnPlane;
+			GetProjParams(c, out camMinOnPlane, out camMaxOnPlane);
+
+			// to local
+			var camMin = transform.InverseTransformPoint(camMinOnPlane);
+			var camMax = transform.InverseTransformPoint(camMaxOnPlane);
+
+			// to grid, using unit quad, so scale is the grid size
+			tileX0 = Mathf.FloorToInt(camMin.x);
+			tileY0 = Mathf.FloorToInt(camMin.z);
+			tileX1 = Mathf.CeilToInt(camMax.x);
+			tileY1 = Mathf.CeilToInt(camMax.z);
+		}
+
+		void RefreshTiles(int tileX0, int tileY0, int tileX1, int tileY1)
+		{
+			Debug.Assert(tileProvider != null);
+
+
+			for (int j = tileY0; j < tileY1; ++j)
+			{
+				for (int i = tileX0; i < tileX1; ++i)
+				{
+					var tile = tileProvider.GetTile(i, j);
+					var xInTile = i - tileX0;
+					var yInTile = j - tileY0;
+					var index = xInTile + yInTile * meshTileYCount;
+					materials[index].SetTexture(ID_MainTex, tile);
+				}
+			}
+
+		}
+
+		// update tile
+		int observingTileX0 = int.MinValue, observingTileY0 = int.MinValue;
+		void LateUpdate()
+		{
+			if (tileProvider != null)
+			{
+				if (tileProvider.showAllTiles)
+				{
+					if (allTilesMeshObj == null)
+					{
+						CreateAllTiles();
+					}
+					if (allTilesMeshObj != null && !allTilesMeshObj.activeSelf)
+					{
+						allTilesMeshObj.SetActive(true);
+					}
+					if (meshObj.activeSelf)
+					{
+						meshObj.SetActive(false);
+					}
+				}
+				else
+				{
+					if (!meshObj.activeSelf)
+					{
+						meshObj.SetActive(true);
+					}
+
+					int tileX0, tileY0, tileX1, tileY1;
+					GetTileParams(out tileX0, out tileY0, out tileX1, out tileY1);
+					if (tileX0 != observingTileX0 || tileY0 != observingTileY0)
+					{
+						observingTileX0 = tileX0;
+						observingTileY0 = tileY0;
+						var p = new Vector3(observingTileX0, 0, observingTileY0);
+						meshObj.transform.localPosition = p;
+						RefreshTiles(tileX0, tileY0, tileX0 + meshTileXCount, tileY0 + meshTileYCount);
+					}
+				}
+			}
+
+		}
+
+		void OnDrawGizmos()
+		{
+			if (Application.isPlaying)
+			{
+				int tileX0, tileY0, tileX1, tileY1;
+				GetTileParams(out tileX0, out tileY0, out tileX1, out tileY1);
+				var gridSize = transform.lossyScale;
+				Gizmos.color = Color.yellow;
+				for (int j = tileY0; j < tileY1; j++)
+				{
+					for (int i = tileX0; i < tileX1; i++)
+					{
+						Gizmos.DrawWireCube(new Vector3(gridSize.x * i, 0, gridSize.z * j) + transform.position + gridSize * 0.5f, gridSize);
+					}
+				}
+			}
+		}
+	}
+}
