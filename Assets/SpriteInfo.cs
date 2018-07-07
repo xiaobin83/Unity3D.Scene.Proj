9ff6fc65d14b28ddcdd92058c0ae5c1b4d3ac69d
xiaobin83 <xiaobin.huang@gmail.com>
2018-07-07 18:58:50 +0800

    mess here, dont touch

---
+++ Assets/SpriteInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace x600d1dea.scene
+{
+
+	[ExecuteInEditMode, RequireComponent(typeof(SpriteRenderer))]
+	public class SpriteInfo : MonoBehaviour
+	{
+		SpriteRenderer spr;
+
+		void OnEnable()
+		{
+			spr = GetComponent<SpriteRenderer>();
+		}
+
+		Vector2 scrollPos = Vector2.zero;
+		void OnGUI()
+		{
+			var bounds = spr.bounds;
+			var p = Camera.main.WorldToScreenPoint(bounds.center + bounds.extents);
+			p.y = Screen.height - p.y;
+			GUILayout.BeginArea(new Rect(p, new Vector2(200, 200)));
+			GUILayout.Label("SpriteRenderer info");
+			scrollPos = GUILayout.BeginScrollView(scrollPos, false, true);
+			GUILayout.Label("spr bounds: " + bounds.ToString());
+			GUILayout.Label("sp bounds: " + spr.sprite.border.ToString());
+			GUILayout.EndScrollView();
+			GUILayout.EndArea();
+		}
+
+	}
+}
